bb35c680dc24cadc0cb230e7ab149635376d88d5
Era-Aich <eraaich488@gmail.com>
2022-01-03 00:18:50 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const changeBoxRedcolor=()=> {
+    document.getElementById("box").style.backgroundColor="red"
+}
+
+const changeBoxbluecolor=()=>{
+    document.getElementById("box").style.backgroundColor="blue"
+}
+
+const changeBoxyellowcolor=()=>{
+    document.getElementById("box").style.backgroundColor="yellow"
+}
